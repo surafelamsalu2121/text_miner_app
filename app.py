a801b2066e2394c36fa25d6d7e1085c76758fe56
Surafel Amsalu <surafel@amplitude.ventures>
2024-11-13 20:30:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,6 @@ def create_app():
 
     return app
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+if __name__ == '__main__':
+    if os.getenv("FLASK_ENV") == "development":
+        app.run(debug=True, host='0.0.0.0', port=5000)
